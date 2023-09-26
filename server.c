e0b943e1da7340d3445f4693c8d62a6b8a08879c
Jatin Vijay <jatin@bjmac.local>
2023-09-26 17:04:15 +0530

    Changes

--- server.c
+++ server.c
@@ -22,13 +22,22 @@ int main() {
 	while(1){
 		msgrcv(msqid, &rcv_msg, sizeof(rcv_msg.mtext), 0, 0);
 		snd_msg.mtype = rcv_msg.mtype;
-		int fd[2];
-		pipe(fd);
+		int fd1[2];
+		int fd2[2];
+		pipe(fd1);
+		pipe(fd2);
 		int isParent = fork();
 		if(isParent){
-			close(fd[0]);
-			write(fd[1], rcv_msg.mtext, sizeof(rcv_msg.mtext));
-			close(fd[1]);
+			close(fd1[0]);
+			write(fd1[1], rcv_msg.mtext, sizeof(rcv_msg.mtext));
+			close(fd1[1]);
+			wait(NULL);
+			close(fd2[1]);
+			read(fd2[0], snd_msg.mtext, sizeof(snd_msg.mtext));
+			close(fd2[0]);
+			msgsnd(msqid, &snd_msg, sizeof(snd_msg.mtext), 0);
+
+
 			continue;
 		}
 
@@ -36,13 +45,13 @@ int main() {
 
 		else{
 			char prompt[100];
-			close(fd[1]);
-			read(fd[0], prompt, sizeof(prompt));
-			close(fd[0]);
+			close(fd1[1]);
+			read(fd1[0], prompt, sizeof(prompt));
+			close(fd1[0]);
 			if(strcmp("hi",prompt) == 0){
-				
-				strcpy(snd_msg.mtext, "hello");
-				
+				close(fd2[0]);
+				write(fd2[1], "hello", sizeof("hello"));
+				close(fd2[1]);				
 				exit(0);
 				}
 			else if (prompt[0] == '2')
@@ -51,8 +60,12 @@ int main() {
 				memmove(prompt, prompt+2, strlen(prompt));
 				strcat(command, prompt);
 				strcpy(snd_msg.mtext, command);
+				dup2 (fd2[1], STDOUT_FILENO);
+				close(fd2[0]);
+				close(fd2[1]);
 				
 				execl("/bin/sh", "sh", "-c", command, NULL);
+				exit(0);
 			}
 			else if (prompt[0] == '3')
 			{	
@@ -60,6 +73,9 @@ int main() {
 				memmove(prompt, prompt+2, strlen(prompt));
 				strcat(command, prompt);
 				strcpy(snd_msg.mtext, command);
+				dup2 (fd2[1], STDOUT_FILENO);
+				close(fd2[0]);
+				close(fd2[1]);
 				execl("/bin/sh", "sh", "-c", command, NULL);
 				
 				exit(0);
