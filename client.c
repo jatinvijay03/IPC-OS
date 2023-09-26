e7da93ef1c4e0f0a46a32f6158c11d463e19a11d
Jatin Vijay <jatin@bjmac.local>
2023-09-26 12:49:46 +0530

    initial commit

---
+++ client.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+
+struct message {
+	long mtype;
+	char mtext[100];
+};message;
+
+int main() {
+	key_t key = ftok("path_to_key_file", 65);
+	int msqid = msgget(key, 0666 | IPC_CREAT);
+
+	int id;
+	printf("Enter client id: ");
+	scanf("%d", &id);
+
+	message.mtype = id;
+	while(1){
+		printf("1. Enter 1 to contact the Ping Server\n2. Enter 2 to contact the File Search Server\n3. Enter 3 to contact the File Word Count Server\n4. Enter 4 if this Client wishes to exit\nYour choice: ");
+		char choice;
+		scanf("%s", &choice);
+		if(choice == '4'){
+			break;
+		}
+		msgsnd(msqid, &message, sizeof(message.mtext), 0);
+		
+	}
+	
+	
+	
+
+	
+    
+
+	return 0;
+}
+
---
+++ server.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+
+struct message {
+	long mtype;
+	char mtext[100];
+};
+
+int main() {	
+	key_t key = ftok("path_to_key_file", 65);
+	int msqid = msgget(key, 0666 | IPC_CREAT);
+
+	struct message rcv_msg;
+	while(1){
+		msgrcv(msqid, &rcv_msg, sizeof(rcv_msg.mtext), 0, 0);
+
+		printf("Received message: %d\n", rcv_msg.mtext);
+	}
+	
+    while(1);
+
+	return 0;
+}
